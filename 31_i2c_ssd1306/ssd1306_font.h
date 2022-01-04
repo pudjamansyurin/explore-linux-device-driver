3eecf6624b498acf99e9b0613a9802d951c41c5e
Puja Kusuma <pujakusumae@hotmail.com>
2022-01-04 15:37:15 +0700

    Added font

---
+++ 31_i2c_ssd1306/ssd1306_font.h
@@ -0,0 +1,8 @@
+#ifndef __SSD1306_FONT_H__
+#define __SSD1306_FONT_H__
+
+#define SSD1306_FONT_SIZE (5)
+
+extern const unsigned char SSD1306_Font[][SSD1306_FONT_SIZE];
+
+#endif /* __SSD1306_FONT_H__ */
